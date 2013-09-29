a6cd0e02bcbc7edb8e5709fcf54882ea7e82eb88
Marc Villacorta <marc.villacorta@gmail.com>
2013-09-29 12:46:59 +0200

    Initializing project

---
+++ src/nre_main.c
@@ -0,0 +1,37 @@
+/******************************************************************************
+* Copyright (C) 2013 Marc Villacorta Morera
+*
+* Authors: Marc Villacorta Morera <marc.villacorta@gmail.com>
+*
+* This file is part of nregexec (nre).
+*
+* Nregexec is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Nregexec is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with nregexec. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+//-----------------------------------------------------------------------------
+// Includes:
+//-----------------------------------------------------------------------------
+
+#include "nre_main.h"
+
+//-----------------------------------------------------------------------------
+// Entry point:
+//-----------------------------------------------------------------------------
+
+int main(int argc, char *argv[])
+
+{
+    printf("Hello NRegExec!\n");
+    return 0;
+}
---
+++ src/nre_main.h
@@ -0,0 +1,39 @@
+/******************************************************************************
+* Copyright (C) 2013 Marc Villacorta Morera
+*
+* Authors: Marc Villacorta Morera <marc.villacorta@gmail.com>
+*
+* This file is part of nregexec (nre).
+*
+* Nregexec is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Nregexec is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with nregexec. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+//-----------------------------------------------------------------------------
+// Include guard:
+//-----------------------------------------------------------------------------
+
+#ifndef _NRE_
+#define _NRE_
+
+//-----------------------------------------------------------------------------
+// Includes:
+//-----------------------------------------------------------------------------
+
+#include <stdio.h>
+
+//-----------------------------------------------------------------------------
+// End of include guard:
+//-----------------------------------------------------------------------------
+
+#endif
